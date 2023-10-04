027f44c1b3b36caba8b002f52bd78bc25f8b6559
Mathias Lykholt-Ustrup <saltworkermlu@gmail.com>
2023-10-04 13:41:40 +0200

    Update Zumo32U4Modules.h

--- Zumo32U4Modules.h
+++ Zumo32U4Modules.h
@@ -1,185 +1,262 @@
-#ifndef Zumo32U4Modules_h   /* Denne linje kommer altid først i en header fil */
+#ifndef Zumo32U4Modules_h   /* This line always comes first in a header file */
 
-#include <Arduino.h>  // En header fil, oprindeligt fra C, påkræver librariet som gør .ino hvad det er.
-#include <Wire.h>     // Zumo32U4.h er afhængig af følgende library
-#include <Zumo32U4.h> // Tilgå Zumo32U4.h library her: https://pololu.github.io/zumo-32u4-arduino-library/
+#include <Arduino.h>  // A header file, originally from C, required the library that makes .ino what it is.
+#include <Wire.h>     // Zumo32U4.h depends on this library to function properly
+#include <Zumo32U4.h> // Access Zumo32U4.h library here: https://pololu.github.io/zumo-32u4-arduino-library/
 
 #define Zumo32U4ModulesButtonBuzzer_h
 /*! \file Zumo32U4ModulesButtonBuzzer.h */
 
-/*! \brief Denne klasse repræsenterer knapperne A, B & C samt Buzzeren
- *
- * Gør det muligt at anvende knapperne A, B & C og spille frekvenser af lyd med Buzzeren.
- * Ud fra disse komponenter der der blevet lavet denne funktion:
- * buttonBootup(attention, windup): Vent på enten A, B eller C trykkes. 
-   Efterfølgende går der {windup} ms inden der gås videre til næste sekvens i programmet.
-   Undervejs lyses der med LEDs og spilles med Buzzer for at indikere programmets start.
-   Og nå jo: Det er muligt at indstille hvad programmet skal gøre afhængigt af hvilken knap trykkes!
-   A returnerer 1; B returnerer 2; C returnerer 3;
- */
+/*! \brief This class represents the Zumo32U4 buttons A, B & C aswell as the buzzer */
 class Zumo32U4ModulesButtonBuzzer : protected Zumo32U4ButtonA, 
                                     protected Zumo32U4ButtonB,
                                     protected Zumo32U4ButtonC, 
                                     protected Zumo32U4Buzzer { 
 public: 
-/*! Vent på enten A, B eller C trykkes. 
-   Efterfølgende går der {windup} ms inden der gås videre til næste sekvens i programmet.
-   Undervejs lyses der med LEDs og spilles med Buzzer for at indikere programmets start.
-   Og nå jo: Det er muligt at indstille hvad programmet skal gøre afhængigt af hvilken knap trykkes!
-   A returnerer 1; B returnerer 2; C returnerer 3; */
-  int buttonBootup(int attention=10, int windup=800); };
+/*! Check button press state.
+ *  NONE=0; A=1; B=2; A+B=3; C=4; A+C=5; B+C=6; A+B+C=7; */
+  int checkButtonPress();
+
+/*! Wait until either A, B or C has been pressed and released.
+    A returns 1; B returns 2; C returns 3 */
+  int getButtonRelease();
+
+/*! Play {frequency} with the buzzer. You can set {duration} and {volume} of the frequency */
+  void buzzer(int frequency=400, int duration=50, int volume=10);
+
+/*! Wait until either A, B or C has been pressed and released.
+   Afterwards, {windup} ms of a "fancy delay" follows.
+   This "fancy delay" consists of the LEDs lighting like a traffic light and the buzzer speaking accordingly.
+   Furthermore: You can set a program protocol depending on button press&release! 
+   Again, A returns 1; B returns 2; C returns 3*/
+  int buttonBootup(int attention=10, int windup=800); 
+};
 
 #define Zumo32U4ModulesMotors_h
 /*! \file Zumo32U4ModulesMotors.h */
 
-/*! \brief Denne klasse repræsenterer de 2 motorer.
- *
- * Kørsel med Zumo32U4 sker med disse 2 funktioner:
- * * motorDrive(left=0, right=0): Kør mellem -400 og 400 med {left} og {right}
- * * motorReverse(bakgear): Skal Zumo32U4 gå i bakgear - bytte rundt på værdierne på begge motorer?
- */
+/*! \brief This class represents the 2 Zumo32U4 motors. */
 class Zumo32U4ModulesMotors : protected Zumo32U4Motors { 
 public: 
-  /*! Kør mellem -400 og 400 med {left} og {right} */
-  void motorDrive(int left=0, int right=0); 
-
-  /*! Skal Zumo32U4 gå i bakgear - bytte rundt på værdierne på begge motorer? */
-  void motorReverse(bool bakgear); 
+  /*! Drive between -400 and 400 with {left} and {right}. If {reverse} is true, motor speeds are reversed */
+  void motorDrive(int left=0, int right=0, bool reverse=false); 
 };
 
 #define Zumo32U4ModulesEncoders_h
 /*! \file Zumo32U4ModulesEncoders.h */
 
-/*! \brief Denne klasse repræsenterer encoders, aka. motorernes omdrejningsmåller
- *
- * Aflæs omdrejninger på Zumo32U4's hjul. Kan ske på 2 måder:
- * * motorOmdrejninger(index): Udvind Omdrejninger på false=venstre & true=højre motor
- * * motorOmdrejningerReset(index): Ligesom motorOmdrejninger(index), bare denne encoder også nulstilles
- */
+/*! \brief This class represents the encoders, aka. the motor turns, based on wheel circumference, measurer */
 class Zumo32U4ModulesEncoders : protected Zumo32U4Encoders { 
 public: 
-  /*! \brief Udvind Omdrejninger på false=venstre & true=højre motor */
-  float motorOmdrejninger(bool index);
+  /*! \brief Get distance in cm of {index}==false: left motor; index==true: right motor */
+  float motorDistance(bool index);
 
-  /*! \brief Ligesom motorOmdrejninger(index), bare denne encoder også nulstilles */
-  float motorOmdrejningerReset(bool index);
+  /*! \brief Like motorOmdrejninger(index), except both encoders are reset */
+  float motorDistanceReset(bool index);
 };
 
 #define Zumo32U4ModulesSensors_h
 /*! \file Zumo32U4ModulesSensors.h */
 
-/*! \brief Denne klasse repræsenterer sensorerne LineSensors, IMU & ProximitySensors
- *
- * Gør det muligt at anvende LineSensors, IMU & ProximitySensors ved blot at konstruere et objekt:
- * * Lave et objekt ud fra denne klasse: Zumo32U4ModulesSensors {OBJEKTNAVN};
- * * Lave et objekt ud fra en klasse hvor nedarvning af denne funktion foretages.
- * Efter objektet er konstrueret vha. constructoren er det blot et spørgsmål om at udvinde værdierne.
- * * getProximitySensorValue(index): Udvind integer fra 0-6 af "proximitet" ift Zumo32U4´s front.
- * * getLineSensorValue(index): Udvind lineSensorValues[{index}], hvor {index} ligger mellem 0 og 2. Ellers: Aflæs LineSensor
- * * getIMUValue(mag): Opdatér alle, hvis ingen parametre indtastes, eller kun én af følgende aflæste sensorer:
- *   - Accelerometer {acc[3]}
- *   - Gyrometer {gyro[3]}
- *   - Magnetometer {mag[3]}
- */
+/*! \brief This class represents the LineSensors, IMU & ProximitySensors
+ * Class constructor enables lineSensors, IMU & ProximitySensors. */
 class Zumo32U4ModulesSensors :  protected Zumo32U4ProximitySensors,
                                 protected Zumo32U4LineSensors, 
                                 protected Zumo32U4IMU {
 public:
   uint16_t lineSensorValues[3];
+  int16_t* mag[3] = {&m.x, &m.y, &m.z};
   int16_t* acc[3] = {&a.x, &a.y, &a.z}; 
   int16_t* gyro[3] = {&g.x, &g.y, &g.z};
-  int16_t* mag[3] = {&m.x, &m.y, &m.z};
-  /*! Eksekveres funktionen {imu.init();} forårsages USB enumeration failure (en dårlig ting).
-   *  Zumo32U4ModulesSensors::type = Zumo32U4IMUType::LSM303D_L3GD20H; // WHY imu.type PRIVATE???*/
+  int16_t gyroOffset;  // When gyro[index] = gyroOffset: No change in angle
+  uint16_t LastUpdate; // Earlier measurement of time with micros(). "Old time"
+  uint32_t turnAngle = 0; // Current calibrated angle of Zumo32U4
+
+  /*! {imu.init();}, a command needed to actually initialize the IMU, causes USB enumeration failure when executed in constructor (Don't try it).
+   *  Zumo32U4ModulesSensors::type = Zumo32U4IMUType::LSM303D_L3GD20H; // Zumo32U4IMUType is private, thus cannot be changed outside the class */
   Zumo32U4ModulesSensors();
   
-  /*! Udvind lineSensorValues[{index}], hvor {index} ligger mellem 0 og 2. Ellers: Aflæs LineSensor */
+  /*! Read proximitySensor. Get right proximitySensor if {index} == false and left if {index} == true */
   int getProximitySensorValue(bool index);
   
-  /*! Udvind lineSensorValues[{index}], hvor {index} ligger mellem 0 og 2. Ellers: Aflæs LineSensor */
+  /*! Read lineSensor. Get lineSensorValues[{index}] provided {index} lies between 0 and 2 */
   int getLineSensorValue(int index=-1);
 
-  /*! Opdatér alle, hvis ingen parametre indtastes, eller kun én af følgende aflæste sensorer:
+  /*! Update all, if no parameter is given, otherwise update one of the following:
  *   - Accelerometer {acc[3]}
  *   - Gyrometer {gyro[3]}
  *   - Magnetometer {mag[3]}
  */
-  int16_t getIMUvalue(char mag='_');
+  int16_t getIMUvalue(char m_a_g='_');
+
+  /*! Needed to get actual angle of Zumo32U4 in setup(). */
+  int16_t calibrate(char m_a_g, int index, int iterations=1000); // EXPERIMENTAL
+
+  /*! After calibrate(char m_a_g, int index, int iterations) has been executed, get actual angle using gyrometer */
+  uint32_t dAngle(int index); // EXPERIMENTAL
 };
 
 #define Zumo32U4ModulesSensors_h
 /*! \file Zumo32U4Modules.h */
 
-/*! \brief En samlet nedarvning - samling - af klasserne for de forskellige Zumo32U4-komponenter.
- *
- * Benytter de 3 knapper, buzzer, motors, encoders, lineSensor, IMU & proximitySensor.
- * Derudover er der følgende funktioner tilkoblet:
- * * LEDBlink(interval): Blink med de 3 LEDs i intervaller af {interval}
- * * IMUEndCondition(): Afslut programmet hvis enten Zumo løftes hurtigt op eller vendes på hovedet
- */
+/*! \brief A class collectively inheriting from the prior classes, essentially uniting all classes into one.
+ * Uses the 3 buttons, buzzer, motors, encoders, lineSensor, IMU & proximitySensor. */
 class Zumo32U4Modules : public Zumo32U4ModulesButtonBuzzer, 
                         public Zumo32U4ModulesMotors, 
                         public Zumo32U4ModulesEncoders, 
-                        public Zumo32U4ModulesSensors
-{
-protected:
-  int displayLine = 0; // Bruges eksklusivt til class Zumo32U4ModulesLCD & class Zumo32U4ModulesOLED
+                        public Zumo32U4ModulesSensors {
 public:
-  /*! Blink med de 3 LEDs i intervaller af {interval}.
-   *  Se funktionen som en mere fancy delay() funktion */
+  int displayLine = 0; // Exclusively used for class Zumo32U4ModulesLCD & class Zumo32U4ModulesOLED
+
+  /*! Blink with the 3 LEDs in intervals of {interval}.
+   *  Consider this function a fancy equivalent to delay() */
   void LEDblink(int interval); 
 
-  /*! Afslut programmet hvis enten Zumo løftes hurtigt op eller vendes på hovedet */
+  /*! End the program if either Zumo is lifted up or turned upside down */
   void IMUEndCondition();
 };
 
 #define Zumo32U4ModulesLCD_h
 /*! \file Zumo32U4ModulesLCD.h */
 
-/*! \brief Benytter komponenterne fra {Zumo32U4Modules} samt LCD-displayet
- *
- * Til at interagere med OLED-displayet er der 2 funktioner klar til brug:
- * * LCDDisplayMenu(): Viser batteriets spænding samt hvorvidt Zumo32U4 er forbundet til en seperat enhed
- * * LCDprint(input, newLine, clear): Print et {input}. En ny linje kan laves inden da med {NewLine}, 
- *   og efterfølgende kan displayet ryddes med {clear}.
- * Eftersom LCD-display er begrænset til 8x2 tegn, kan der vitterligt kun stå 16 tegn på displayet ad gangen.
- */
+/*! \brief Use components from {Zumo32U4Modules} aswell as the LCD-display.
+ * The LCD-display is limited to 8x2 dimensions, meaning only 16 signs can be displayed once at a time. */
 class Zumo32U4ModulesLCD : public Zumo32U4Modules, protected Zumo32U4LCD {
 public:
-  /*! Eftersom LCD-display er begrænset til 8x2 tegn, kan der vitterligt kun stå 16 tegn på displayet ad gangen. */
+  /*! Equivalent to a "Hello World!" displayed on the LCD. */
   Zumo32U4ModulesLCD();
 
-  /*! Viser batteriets spænding samt hvorvidt Zumo32U4 er forbundet til en seperat enhed. */
-  void LCDDisplayMenu();
+  /*! Show battery voltage aswell as whether Zumo32U4 is connected to another unit. */
+  void displayMenu();
 
-  /*! Print et {input}. En ny linje kan laves inden da med {NewLine}, og efterfølgende kan displayet ryddes med {clear}. */
-  void LCDprint(String input, bool newLine=false, bool clear=false);
+  /*! Print {input}. {clear} determines whether the display should be cleared prior. {NewLine} makes the next displayPrint(char input[]) be printed on the next line */
+  void displayPrint(char input[], bool clear=false, bool newLine=true); // Example: LCDprint("Hello World!");
+
+  /*! Set up to 8 custom characters on the dispaly. By default, 8 custom characters are defined in class constructor.
+   *  Try defining your own custom character by inserting the following into your .ino file: 
+   *  const char FULLBAR[] PROGMEM = {63, 63, 63, 63, 63, 63, 63, 63}; */
+  void displayCustomCharacters(char custom1[]={}, char custom2[]={}, char custom3[]={}, char custom4[]={}, 
+                               char custom5[]={}, char custom6[]={}, char custom7[]={}, char custom8[]={});
 };
 
 #define Zumo32U4ModulesOLED_h
 /*! \file Zumo32U4ModulesOLED.h */
 
-/*! \brief Benytter komponenterne fra {Zumo32U4Modules} samt OLED-displayet
- *
- * Til at interagere med OLED-displayet er der 2 funktioner klar til brug:
- * * OLEDDisplayMenu(): Viser batteriets spænding samt hvorvidt Zumo32U4 er forbundet til en seperat enhed
- * * OLEDprint(input, newLine, clear): Print et {input}. En ny linje kan laves inden da med {NewLine}, 
- *   og efterfølgende kan displayet ryddes med {clear}.
- * Desuden er der muligheden for at lave vfx (graphics) [WORK IN PROGRESS]
- */
-
-
+/*! \brief Use components from {Zumo32U4Modules} aswell as the OLED-display.
+ * The OLED-display can change the display dimensions to 8x2, 11x4 & 21x8.
+ * Furthermore, the OLED-display is capable of making vfx graphics [WORK IN PROGRESS] */
 class Zumo32U4ModulesOLED : public Zumo32U4Modules, protected Zumo32U4OLED {
 public:
-  /*! OLED-displayet kan have op til 21x8 (ellers 11x4 & 8x2) tegn på displayet på samme tid! 
-   *  Desuden er der muligheden for at lave vfx (graphics) [WORK IN PROGRESS] */
+  /*! Equivalent to a "Hello World!" displayed on the LCD. */
   Zumo32U4ModulesOLED();
 
-  /*! Viser batteriets spænding samt hvorvidt Zumo32U4 er forbundet til en seperat enhed. */
-  void OLEDDisplayMenu();
+  /*! Show battery voltage aswell as whether Zumo32U4 is connected to another unit. */
+  void displayMenu();
 
-  /*! Print et {input}. En ny linje kan laves inden da med {NewLine}, og efterfølgende kan displayet ryddes med {clear}. */
-  void OLEDprint(String input, bool newLine=false, bool clear=false);
+  /*! Print {input}. {clear} determines whether the display should be cleared prior. {NewLine} makes the next displayPrint(char input[]) be printed on the next line */
+  void displayPrint(char input[], bool clear=false, bool newLine=true); // Eksempel: OLEDprint("Hello World!");
+
+  /*! Set up to 8 custom characters on the dispaly. By default, 8 custom characters are defined in class constructor.
+   *  Try defining your own custom character by inserting the following into your .ino file: 
+   *  const char FULLBAR[] PROGMEM = {63, 63, 63, 63, 63, 63, 63, 63}; */
+  void displayCustomCharacters(char custom1[]={}, char custom2[]={}, char custom3[]={}, char custom4[]={}, 
+                               char custom5[]={}, char custom6[]={}, char custom7[]={}, char custom8[]={});
 };
 
-#endif // Denne linje kommer altid sidst i en header fil
+// This character is a back arrow ultimately pointing to the left.
+const char backArrow[] PROGMEM = {
+  0b00000,
+  0b00010,
+  0b00001,
+  0b00101,
+  0b01001,
+  0b11110,
+  0b01000,
+  0b00100,
+};
+
+// This character is a back arrow ultimately pointing to the right.
+const char backArrowReverse[] PROGMEM = {
+  0b00000,
+  0b01000,
+  0b10000,
+  0b10100,
+  0b10010,
+  0b01111,
+  0b00010,
+  0b00100,
+};
+
+// This character is two chevrons pointing up.
+const char forwardArrows[] PROGMEM = {
+  0b00000,
+  0b00100,
+  0b01010,
+  0b10001,
+  0b00100,
+  0b01010,
+  0b10001,
+  0b00000,
+};
+
+// This character is two chevrons pointing down.
+const char reverseArrows[] PROGMEM = {
+  0b00000,
+  0b10001,
+  0b01010,
+  0b00100,
+  0b10001,
+  0b01010,
+  0b00100,
+  0b00000,
+};
+
+// This character is two solid arrows pointing up.
+const char forwardArrowsSolid[] PROGMEM = {
+  0b00000,
+  0b00100,
+  0b01110,
+  0b11111,
+  0b00100,
+  0b01110,
+  0b11111,
+  0b00000,
+};
+
+// This character is two solid arrows pointing down.
+const char reverseArrowsSolid[] PROGMEM = {
+  0b00000,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b11111,
+  0b01110,
+  0b00100,
+  0b00000,
+};
+
+// This character is a solid arrow pointing to the right.
+const char rightArrow[] PROGMEM = {
+  0b01000,
+  0b01100,
+  0b01110,
+  0b01111,
+  0b01110,
+  0b01100,
+  0b01000,
+  0b00000,
+};
+
+// This character is a solid arrow pointing to the left.
+const char leftArrow[] PROGMEM = {
+  0b00010,
+  0b00110,
+  0b01110,
+  0b11110,
+  0b01110,
+  0b00110,
+  0b00010,
+  0b00000,
+};
+
+#endif // This line always comes last in a header file
